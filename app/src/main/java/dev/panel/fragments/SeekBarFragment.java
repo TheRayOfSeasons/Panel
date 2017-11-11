edaa7000d6bd9f683b7c49231b097817ed907958
TheRayOfSeasons <sison.ray@ciit.edu.ph>
2017-11-11 16:14:36 +0800

    System volume seekbar (in progress)

--- app/src/main/java/dev/panel/fragments/SeekBarFragment.java
+++ app/src/main/java/dev/panel/fragments/SeekBarFragment.java
@@ -1,6 +1,8 @@
 package dev.panel.fragments;
 
 import android.content.ContentResolver;
+import android.content.Context;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
@@ -14,68 +16,75 @@ import dev.panel.R;
 import dev.panel.utils.L;
 
 @SuppressWarnings("RedundantCast")
-public class SeekBarFragment extends Fragment implements SeekBar.OnSeekBarChangeListener
-{
-    private ContentResolver contentResolver;
-
-    public SeekBarFragment() {}
-
-    @Nullable
-    @Override
-    public View onCreateView
-        (
-            LayoutInflater inflater,
-            @Nullable ViewGroup container,
-            @Nullable Bundle savedInstanceState
-        )
+    public class SeekBarFragment extends Fragment implements SeekBar.OnSeekBarChangeListener
     {
-        contentResolver = getContext().getContentResolver();
+        private ContentResolver contentResolver;
+        private AudioManager systemAudio = null;
 
-        View view = inflater.inflate(R.layout.fragment_seekbars, container, false);
+        public SeekBarFragment() {}
 
-        //TODO: Update when brightness is changed by other apps
-        SeekBar brightnessBar =  (SeekBar) view.findViewById(R.id.brightness_bar);
-        brightnessBar.setMax(255);
-        brightnessBar.setProgress(getScreenBrightness());
-        brightnessBar.setOnSeekBarChangeListener(this);
-
-        SeekBar systemVolumeBar = (SeekBar) view.findViewById(R.id.system_volume_bar);
-        systemVolumeBar.setMax(100);
-        systemVolumeBar.setOnSeekBarChangeListener(this);
-
-        SeekBar mediaVolumeBar = (SeekBar) view.findViewById(R.id.media_volume_bar);
-        mediaVolumeBar.setMax(100);
-        mediaVolumeBar.setOnSeekBarChangeListener(this);
-
-        return view;
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState)
-    {
-        super.onActivityCreated(savedInstanceState);
-    }
-
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int i, boolean b)
-    {
-        switch (seekBar.getId())
+        @Nullable
+        @Override
+        public View onCreateView
+            (
+                LayoutInflater inflater,
+                @Nullable ViewGroup container,
+                @Nullable Bundle savedInstanceState
+            )
         {
-            case R.id.brightness_bar:
-                L.m("b", i);
+            contentResolver = getContext().getContentResolver();
 
-                Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, i);
-                break;
+            View view = inflater.inflate(R.layout.fragment_seekbars, container, false);
 
-            case R.id.system_volume_bar:
-                L.m("sv", i);
-                break;
+            //TODO: Update when brightness is changed by other apps
+            SeekBar brightnessBar =  (SeekBar) view.findViewById(R.id.brightness_bar);
+            brightnessBar.setMax(255);
+            brightnessBar.setProgress(getScreenBrightness());
+            brightnessBar.setOnSeekBarChangeListener(this);
 
-            case R.id.media_volume_bar:
-                L.m("mv", i);
-                break;
+            SeekBar systemVolumeBar = (SeekBar) view.findViewById(R.id.system_volume_bar);
+            systemVolumeBar.setMax(systemAudio.getStreamMaxVolume(AudioManager.STREAM_SYSTEM));
+            systemVolumeBar.setProgress(systemAudio.getStreamVolume(AudioManager.STREAM_SYSTEM));
+            systemVolumeBar.setOnSeekBarChangeListener(this);
+
+            SeekBar mediaVolumeBar = (SeekBar) view.findViewById(R.id.media_volume_bar);
+            mediaVolumeBar.setMax(100);
+            mediaVolumeBar.setOnSeekBarChangeListener(this);
+
+            return view;
         }
-    }
+
+        @Override
+        public void onActivityCreated(@Nullable Bundle savedInstanceState)
+        {
+            super.onActivityCreated(savedInstanceState);
+        }
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b)
+        {
+            systemAudio = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+            switch (seekBar.getId())
+            {
+                case R.id.brightness_bar:
+                    L.m("b", i);
+
+                    Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, i);
+                    break;
+
+                case R.id.system_volume_bar:
+                    systemAudio.setStreamVolume(AudioManager.STREAM_SYSTEM, i, 0);
+                    L.m("sv", i);
+                    break;
+
+                case R.id.media_volume_bar:
+                    //TODO Make Functional
+                    L.m("mv", i);
+                    break;
+            }
+        }
+
+
 
     @Override public void onStartTrackingTouch(SeekBar seekBar) {}
     @Override public void onStopTrackingTouch(SeekBar seekBar) {}
