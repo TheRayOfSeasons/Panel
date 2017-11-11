a3926ff8b36bb2aec05a6dd9e371f53d313f06f7
esfox <osairis100@gmail.com>
2017-11-11 16:06:03 +0800

    Imported button icons

--- app/src/main/java/dev/panel/fragments/ButtonCaseFragment.java
+++ app/src/main/java/dev/panel/fragments/ButtonCaseFragment.java
@@ -6,7 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageView;
 
 import dev.panel.R;
 import dev.panel.utils.L;
@@ -28,15 +28,16 @@ public class ButtonCaseFragment extends Fragment implements View.OnClickListener
         View view = inflater.inflate(R.layout.fragment_buttoncase, container, false);
 
         //TODO: Change button ID and object names
-        Button button1 = (Button) view.findViewById(R.id.button_1);
-        Button button2 = (Button) view.findViewById(R.id.button_2);
-        Button button3 = (Button) view.findViewById(R.id.button_3);
-        Button button4 = (Button) view.findViewById(R.id.button_4);
-        Button button5 = (Button) view.findViewById(R.id.button_5);
+        ImageView flashlight = (ImageView) view.findViewById(R.id.toggle_flashlight);
+        ImageView volumeModes = (ImageView) view.findViewById(R.id.toggle_volume_modes);
+        ImageView wifi = (ImageView) view.findViewById(R.id.toggle_wifi);
+        ImageView bluetooth = (ImageView) view.findViewById(R.id.toggle_bluetooth);
+        ImageView rotation = (ImageView) view.findViewById(R.id.toggle_rotation);
 
-        Button[] buttons = new Button[] { button1, button2, button3, button4, button5 };
-        for (Button button: buttons)
-            button.setOnClickListener(this);
+        ImageView[] toggles = new ImageView[]
+                { flashlight, volumeModes, wifi, bluetooth, rotation };
+        for (ImageView toggle: toggles)
+            toggle.setOnClickListener(this);
 
         return view;
     }
@@ -46,24 +47,24 @@ public class ButtonCaseFragment extends Fragment implements View.OnClickListener
     {
         switch (view.getId())
         {
-            case R.id.button_1:
-                L.m("Button", "1");
+            case R.id.toggle_flashlight:
+                L.m("toggle", "flashlight");
                 break;
 
-            case R.id.button_2:
-                L.m("Button", "2");
+            case R.id.toggle_volume_modes:
+                L.m("toggle", "volume modes");
                 break;
 
-            case R.id.button_3:
-                L.m("Button", "3");
+            case R.id.toggle_wifi:
+                L.m("toggle", "wifi");
                 break;
 
-            case R.id.button_4:
-                L.m("Button", "4");
+            case R.id.toggle_bluetooth:
+                L.m("toggle", "bluetooth");
                 break;
 
-            case R.id.button_5:
-                L.m("Button", "5");
+            case R.id.toggle_rotation:
+                L.m("toggle", "rotation");
                 break;
         }
     }
